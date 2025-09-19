e6e71065adbd1aadc5f4b99042e1a57ec6c6346f
Pradipta <153537760+pradipta1023@users.noreply.github.com>
2025-09-19 13:11:57 +0530

    Created a file to save a program which gives grade depending on marks

---
+++ if_else.js
@@ -0,0 +1,12 @@
+const marks = 76;
+if(marks > 80) {
+    console.log("Sorry! You're not good at study");
+} else if(marks > 60) {
+    console.log("Okay! But still you're not good at study");
+} else if(marks > 40) {
+    console.log("You're good! Keep it up");   
+} else if(marks < 40) {
+    console.log("Exclelent! You're in a right path...");
+} else {
+    console.log("Something went wrong");
+}
