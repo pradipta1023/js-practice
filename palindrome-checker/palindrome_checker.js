d43bda295fe9de5f82d8901115060252ddcf2868
Pradipta <153537760+pradipta1023@users.noreply.github.com>
2025-09-21 01:36:45 +0530

    Updated file palindrome_checker.js
    Just updated the if-else statements using a loop.

--- palindrome-checker/palindrome_checker.js
+++ palindrome-checker/palindrome_checker.js
@@ -1,28 +1,21 @@
-const input = 12321 + "";
+const input = 0 + "";
 let output = true;
 
 let startOfString = 0;
 let endOfString = input.length-1;
 
-if(input[startOfString] !== input[endOfString]) {
-    output = false;
+let leftDigit;
+let rightDigit;
+while(startOfString <= endOfString) {
+
+    leftDigit = input[startOfString];
+    rightDigit = input[endOfString];
+    if(leftDigit !== rightDigit) {
+        output = false;
+    }
+    startOfString = startOfString + 1;
+    endOfString = endOfString - 1;
 }
 
-startOfString = startOfString + 1;
-endOfString = endOfString - 1;
-
-if(startOfString === endOfString) {
     console.log("Input:",input,", Output:",output);
-}
-
-if(input[startOfString] !== input[endOfString]) {
-    output = false;
-}
-
-startOfString = startOfString + 1;
-endOfString = endOfString - 1;
-
-if(startOfString === endOfString) {
-    console.log("Input:",input,", Output:",output);
-}
 
