bf7e9b96f795d1ac125f0f6a8ec3218766f9f711
Pradipta <153537760+pradipta1023@users.noreply.github.com>
2025-09-21 16:59:30 +0530

    Created file isVowel.js
    Draft version of how to find  a vowel

---
+++ no-of-vowels-in-string/isVowel.js
@@ -0,0 +1,14 @@
+const input = "o";
+let output = false;
+
+if(input === "a") {
+    output = true;
+} else if(input === "e") {
+    output = true;
+} else if(input === "i") {
+    output = true;
+} else if(input === "o") {
+    output = true;
+}
+
+console.log("Input:" ,input, "output:",output);
