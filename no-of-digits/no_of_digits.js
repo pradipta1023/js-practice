da4cf3c2dfd163c96e97a42f67bea80f810a06dd
Pradipta <153537760+pradipta1023@users.noreply.github.com>
2025-09-21 10:31:15 +0530

    Updated file no_of_digits.js
    Updated the draft into while loop

--- no-of-digits/no_of_digits.js
+++ no-of-digits/no_of_digits.js
@@ -1,49 +1,12 @@
 const inputToShow = 197655;
-let output = 0;
+let noOfDigitsInInput = 0;
 
 let inputToUse = inputToShow;
-if(inputToUse === 0){
-	console.log("Input:",inputToShow, "Output:",output);
+let remainder;
+while(inputToUse !== 0) {
+	remainder = inputToUse % 10;
+	inputToUse = (inputToUse - remainder) / 10;
+	noOfDigitsInInput = noOfDigitsInInput + 1;
 }
 
-let remainder = inputToUse % 10;
-inputToUse = (inputToUse - remainder) / 10;
-output = output + 1;
-if(inputToUse === 0){
-	console.log("Input:",inputToShow, "Output:",output);
-}
-
-remainder = inputToUse % 10;
-inputToUse = (inputToUse - remainder) / 10;
-output = output + 1;
-if(inputToUse === 0){
-	console.log("Input:",inputToShow, "Output:",output);
-}
-
-remainder = inputToUse % 10;
-inputToUse = (inputToUse - remainder) / 10;
-output = output + 1;
-if(inputToUse === 0){
-	console.log("Input:",inputToShow, "Output:",output);
-}
-
-remainder = inputToUse % 10;
-inputToUse = (inputToUse - remainder) / 10;
-output = output + 1;
-if(inputToUse === 0){
-	console.log("Input:",inputToShow, "Output:",output);
-}
-
-remainder = inputToUse % 10;
-inputToUse = (inputToUse - remainder) / 10;
-output = output + 1;
-if(inputToUse === 0){
-	console.log("Input:",inputToShow, "Output:",output);
-}
-
-remainder = inputToUse % 10;
-inputToUse = (inputToUse - remainder) / 10;
-output = output + 1;
-if(inputToUse === 0){
-	console.log("Input:",inputToShow, "Output:",output);
-}
+console.log("Input:",inputToShow, "Output:",noOfDigitsInInput);
