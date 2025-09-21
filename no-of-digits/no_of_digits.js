3123cf33d09688d4ad02b526373927034f9ba107
Pradipta <153537760+pradipta1023@users.noreply.github.com>
2025-09-21 10:25:58 +0530

    Created file no_of_digits.js
    Draft file for calculating number of digits is created

---
+++ no-of-digits/no_of_digits.js
@@ -0,0 +1,49 @@
+const inputToShow = 197655;
+let output = 0;
+
+let inputToUse = inputToShow;
+if(inputToUse === 0){
+	console.log("Input:",inputToShow, "Output:",output);
+}
+
+let remainder = inputToUse % 10;
+inputToUse = (inputToUse - remainder) / 10;
+output = output + 1;
+if(inputToUse === 0){
+	console.log("Input:",inputToShow, "Output:",output);
+}
+
+remainder = inputToUse % 10;
+inputToUse = (inputToUse - remainder) / 10;
+output = output + 1;
+if(inputToUse === 0){
+	console.log("Input:",inputToShow, "Output:",output);
+}
+
+remainder = inputToUse % 10;
+inputToUse = (inputToUse - remainder) / 10;
+output = output + 1;
+if(inputToUse === 0){
+	console.log("Input:",inputToShow, "Output:",output);
+}
+
+remainder = inputToUse % 10;
+inputToUse = (inputToUse - remainder) / 10;
+output = output + 1;
+if(inputToUse === 0){
+	console.log("Input:",inputToShow, "Output:",output);
+}
+
+remainder = inputToUse % 10;
+inputToUse = (inputToUse - remainder) / 10;
+output = output + 1;
+if(inputToUse === 0){
+	console.log("Input:",inputToShow, "Output:",output);
+}
+
+remainder = inputToUse % 10;
+inputToUse = (inputToUse - remainder) / 10;
+output = output + 1;
+if(inputToUse === 0){
+	console.log("Input:",inputToShow, "Output:",output);
+}
