00142b0c2ff3f7643bec42a4cf05454bee350f7a
Pradipta <153537760+pradipta1023@users.noreply.github.com>
2025-11-05 18:50:49 +0530

    Create first_ten_odd_numbers.js

---
+++ jsPrograms/programFiles/first_ten_odd_numbers.js
@@ -0,0 +1,113 @@
+// let count = 0;
+// let input = 11;
+// let output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+// count = count + 1;
+// console.log("Stop if count is 10",count);
+
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+// count = count + 1;
+// console.log("Stop if count is 10",count);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+// count = count + 1;
+// console.log("Stop if count is 10",count);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+// count = count + 1;
+// console.log("Stop if count is 10",count);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+// count = count + 1;
+// console.log("Stop if count is 10",count);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+// count = count + 1;
+// console.log("Stop if count is 10",count);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+// count = count + 1;
+// console.log("Stop if count is 10",count);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+// count = count + 1;
+// console.log("Stop if count is 10",count);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+// count = count + 1;
+// console.log("Stop if count is 10",count);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+
+// input = input + 1;
+// output = (input) % 2 ? input : "";
+// console.log("Input:",input,"output:",output);
+// count = count + 1;
+// console.log("Stop if count is 10",count);
+
+
+const input = 10;
+let targetNumber = input;
+let count = 1;
+let output;
+console.log("Input:",input)
+while(count <= 10){
+    output = (targetNumber) % 2 ? targetNumber : "";
+    if(output){
+        console.log("output:",output);
+        count = count + 1;
+    }
+    targetNumber = targetNumber + 1;
+}
