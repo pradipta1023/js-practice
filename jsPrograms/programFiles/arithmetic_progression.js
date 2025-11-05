1a6a0ada3c0018e114a071cb691c4a9286c8e4c4
Pradipta <153537760+pradipta1023@users.noreply.github.com>
2025-11-05 18:46:58 +0530

    Create arithmetic_progression.js

---
+++ jsPrograms/programFiles/arithmetic_progression.js
@@ -0,0 +1,27 @@
+// let output = 0;
+// input = 2;
+// output = output + input;
+// console.log("Arithmetic Progression of",input,"is",output);
+
+// input = 3;
+// output = output + input;
+// console.log("Arithmetic Progression of",input,"is",output);
+
+// input = 4;
+// output = output + input;
+// console.log("Arithmetic Progression of",input,"is",output);
+
+// input = 5;
+// output = output + 5;
+// console.log("Arithmetic Progression of",input,"is",output);
+
+// input = 6;
+// output = output + 6;
+// console.log("Arithmetic Progression of",input,"is",output);
+
+const number = 6;
+let output = 0;
+for(let input = 1; input <= number; input++) {
+    output = output + input;
+    console.log("Arithmetic Progression of",input,"is",output);
+}
